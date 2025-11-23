964aee65ad0977a6be828e5ac92468e30474865d
oGhartey <ondghartey@gmail.com>
2025-11-23 16:23:57 +0000

    Correction: Moving interfaces to the right directory

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { type LayoutProps } from "@/interface";
+import { LayoutProps } from "@/interface";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
